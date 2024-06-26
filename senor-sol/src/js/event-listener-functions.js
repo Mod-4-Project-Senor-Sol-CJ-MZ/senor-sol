95723f97cba46a70a139d8c6f38bf3e3e4a929ac
Mei Zhu <maggiez0613@gmail.com>
2024-06-26 15:09:35 -0400

    time converter done

--- senor-sol/src/js/event-listener-functions.js
+++ senor-sol/src/js/event-listener-functions.js
@@ -7,7 +7,7 @@ export const handleSubmit = async (event) => {
   const formData = new FormData(form);
   const formObj = Object.fromEntries(formData);
 
-  console.log('Form Data:', formObj);
+  // console.log('Form Data:', formObj);
 
   const zipcodeValue = document.getElementById("results-zipcode");
   zipcodeValue.textContent = "Zipcode: " + formObj.zipcode;
@@ -16,23 +16,38 @@ export const handleSubmit = async (event) => {
   dateValue.textContent = "Date: " + formObj.date;
 
   const timezoneValue = document.getElementById("results-timezone");
+  // try if/else statements here with formObj.timezone ? 
+    // if America/New York -> textContent should be EST ? 
+      // make sense ? 
   timezoneValue.textContent = "Timezone: " + formObj.timezone;
 
   try {
     const { latitude, longitude, city, state } = await getLocationData(formObj.zipcode);
-    console.log(`Location: ${city}, ${state} ${formObj.zipcode}`);
+
+    const location = document.getElementById("results-location");
+    location.textContent = `Location: ${city}, ${state} ${formObj.zipcode}`
 
     const { sunrise, sunset } = await getSunriseSunset(latitude, longitude, formObj.date);
 
     const sunriseValue = document.getElementById("results-sunrise");
-    sunriseValue.textContent = "Sunrise: " + new Date(sunrise).toLocaleTimeString();
+    sunriseValue.textContent = "Sunrise: " + convertToTimezone(sunrise, formObj.timezone);
 
     const sunsetValue = document.getElementById("results-sunset");
-    sunsetValue.textContent = "Sunset: " + new Date(sunset).toLocaleTimeString();
-
+    sunsetValue.textContent = "Sunset: " + convertToTimezone(sunset, formObj.timezone);
   } catch (error) {
     console.warn(error);
   }
 
   form.reset();
+};
+
+const convertToTimezone = (time, timeZone) => {
+  // timeZone connects with option values in the form 
+  const date = new Date(time);
+  return new Intl.DateTimeFormat('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZone: timeZone
+  }).format(date);
 };
